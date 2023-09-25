4cfc8476dc32614ad146e3ff35e23e617f78f0f9
Omelette719 <145272392+Omelette719@users.noreply.github.com>
2023-09-25 20:58:24 +0800

    Add files via upload

---
+++ 2310817310012_Muhammad Azwin Hakim_Tugas2_PraktikumPemrogramanI/C.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    printf("Selamat Pagi,\n");
+    printf("\nTeknologi Informasi FTULM Selalu dihati");
+    return 0;
+}
---
+++ 2310817310012_Muhammad Azwin Hakim_Tugas2_PraktikumPemrogramanI/Python.py
@@ -0,0 +1,2 @@
+print("Selamat Pagi,")
+print("\nTeknologi Informasi FTULM Selalu dihati")
